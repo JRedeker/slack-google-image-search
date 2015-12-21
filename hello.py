d7106ffd89fc6ad37f1df6e96326bbb58e953a46
Jon Redeker <jon.redeker@gmail.com>
2015-12-21 14:51:32 -0500

    change!

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
 import os
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'Hello World'
+
+app.route('/images', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        result = {
+        	"response_type": "in_channel",
+        	"text": "this is the message"
+        }
+        return result
+    else:
+        return
