d03e5c2bfe15abae59eefcffd369e92a0338a0c8
Jon Redeker <jon.redeker@gmail.com>
2015-12-24 13:05:27 -0500

    actually gets image url

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 import os
 import urllib2
 import simplejson
-import cStringIO
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
@@ -14,17 +13,24 @@ def returnJson():
 	#make json
 	#request.query_string
 	request_data = request.form.get('text')
-	imageUrl = getImage(request_data)
+	request_ip = request.remote_addr
+	imageUrl = getImage(request_data,request_ip)
 	#return it
 	return jsonify(response_type='in_channel',text=imageUrl)
 
-def getImage(request_data):
+def getImage(request_data,ip):
 	fetcher = urllib2.build_opener()
-	searchTerm = 'parrot'
+	searchTerm = request_data
 	startIndex = 0
 	searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + searchTerm + "&start=" + startIndex
-	f = fetcher.open(searchUrl)
-	deserialized_output = simplejson.load(f)
-	imageUrl = deserialized_output['responseData']['results'][0]['unescapedUrl']
+	url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+	       'v=1.0&q=barack%20obama&userip=' + ip)
+
+	request = urllib2.Request(url, None, {'Referer': 'https://salty-taiga-1657.herokuapp.com/image'})
+	response = urllib2.urlopen(request)
+
+	# Process the JSON string.
+	results = simplejson.load(response)
+	imageUrl = results['responseData']['results'][0]['unescapedUrl']
 	#file = cStringIO.StringIO(urllib.urlopen(imageUrl).read())
 	return imageUrl
