ba17258c0927b75c8147e5d4a530bc1463243047
Jon Redeker <jon.redeker@gmail.com>
2015-12-24 11:28:38 -0500

    crap

--- hello.py
+++ hello.py
@@ -10,6 +10,6 @@ def hello():
 def returnJson():
 	#make json
 	#request.query_string
-	theText = request.args
+	theText = "Hello!"
 	#return it
 	return jsonify(response_type='in_channel',text=theText)
