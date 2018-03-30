1f4fe9ad849d57c1429f782a0e3735fdd0ce4c15
Ben Marti <William.Marti@gfs.com>
2018-03-29 22:53:37 -0400

    add app.yaml for app engine, add error handling in main.py

--- main.py
+++ main.py
@@ -73,6 +73,11 @@ def getTestImage():
 
 	return first_image_url
 
+@app.errorhandler(500)
+def server_error(e):
+	# Log the error and stacktrace.
+	logging.exception('An error occurred during a request.')
+	return 'An internal error occurred.', 500
 
 # only for testing
-app.run()
+#app.run()
